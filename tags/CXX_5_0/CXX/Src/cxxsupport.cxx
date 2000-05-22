6f55a8f263a9ab757dd1521106a458aad3b6454b
(no author) <(no author)@cb2f5747-f50d-0410-b093-b27f9c363ba5>
2000-05-22 21:40:51 +0000

    This commit was manufactured by cvs2svn to create tag 'CXX_5_0'.
    git-svn-id: http://svn.code.sf.net/p/cxx/code@14 cb2f5747-f50d-0410-b093-b27f9c363ba5

---
+++ tags/CXX_5_0/CXX/Src/cxxsupport.cxx
@@ -0,0 +1,39 @@
+//----------------------------------*-C++-*----------------------------------//
+// Copyright 1998 The Regents of the University of California. 
+// All rights reserved. See Legal.htm for full text and disclaimer.
+//---------------------------------------------------------------------------//
+
+#include "CXX/CXX_Objects.h"
+namespace Py {
+
+    Type Object::type () const { 
+        return Type (PyObject_Type (p), true);
+    }
+    
+    String Object::str () const {
+        return String (PyObject_Str (p), true);
+    }
+    
+    String Object::repr () const { 
+        return String (PyObject_Repr (p), true);
+    }
+    
+    std::string Object::as_string() const {
+        return static_cast<std::string>(str());
+    }
+        
+    bool Object::isType (const Type& t) const { 
+        return type ().ptr() == t.ptr();
+    }
+    
+    Char::operator String() const {
+        return String(ptr());
+    }
+
+    
+    // output
+    std::ostream& operator<< (std::ostream& os, const Object& ob) {
+        return (os << static_cast<std::string>(ob.str()));
+    }  
+
+} // Py
