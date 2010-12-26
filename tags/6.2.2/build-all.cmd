13409ac307458f1dfe6776d51214e9074482d2f8
barry-scott <barry-scott@cb2f5747-f50d-0410-b093-b27f9c363ba5>
2010-12-26 16:03:45 +0000

    opss not finished
    git-svn-id: http://svn.code.sf.net/p/cxx/code@253 cb2f5747-f50d-0410-b093-b27f9c363ba5

--- tags/6.2.2/build-all.cmd
+++
@@ -1,9 +0,0 @@
-setlocal
-setup-msvc90
-c:\python26\python setup_makefile.py win32 win32.mak
-nmake -f win32.mak clean all
-
-c:\python31\python setup_makefile.py win32 win32.mak
-nmake -f win32.mak clean all
-
-endlocal
