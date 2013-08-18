42e32d877d162104253ad6f98861b213a098bf78
barry-scott <barry-scott@cb2f5747-f50d-0410-b093-b27f9c363ba5>
2013-08-18 14:19:08 +0000

    add python 3.3 to tests
    git-svn-id: http://svn.code.sf.net/p/cxx/code@285 cb2f5747-f50d-0410-b093-b27f9c363ba5

--- trunk/CXX/build-all.cmd
+++ trunk/CXX/build-all.cmd
@@ -6,4 +6,7 @@ nmake -f win32.mak clean all
 c:\python32.win32\python setup_makefile.py win32 win32.mak
 nmake -f win32.mak clean all
 
+c:\python33.win32\python setup_makefile.py win32 win32.mak
+nmake -f win32.mak clean all
+
 endlocal
