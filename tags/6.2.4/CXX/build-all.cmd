9b8b9f881619bf6605350263a1861977177a990d
barry-scott <barry-scott@cb2f5747-f50d-0410-b093-b27f9c363ba5>
2013-08-18 13:49:37 +0000

    tagged wrong folder
    git-svn-id: http://svn.code.sf.net/p/cxx/code@283 cb2f5747-f50d-0410-b093-b27f9c363ba5

--- tags/6.2.4/CXX/build-all.cmd
+++
@@ -1,9 +0,0 @@
-setlocal
-call setup-msvc90
-c:\python27.win32\python setup_makefile.py win32 win32.mak
-nmake -f win32.mak clean all
-
-c:\python32.win32\python setup_makefile.py win32 win32.mak
-nmake -f win32.mak clean all
-
-endlocal
