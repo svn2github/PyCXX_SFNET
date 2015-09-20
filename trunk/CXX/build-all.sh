f78d2426bc9e9459bf13356023e8298feed48ae1
barry-scott <barry-scott@cb2f5747-f50d-0410-b093-b27f9c363ba5>
2015-09-20 15:01:40 +0000

    improve the build-all.sh to auto detect the OS it is running on and also stop when a error occurres
    git-svn-id: http://svn.code.sf.net/p/cxx/code@308 cb2f5747-f50d-0410-b093-b27f9c363ba5

--- trunk/CXX/build-all.sh
+++ trunk/CXX/build-all.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
-OS=${1:-macosx}
+set -o pipefail
+
+case "$( uname )" in
+Darwin)
+    OS=macosx
+    ;;
+Linux):
+    OS=linux
+    ;;
+*)
+    echo Unknown OS assuming Linux
+    OS=linux
+    ;;
+esac
 
 for PYTHON in \
     python2.6 \
@@ -8,10 +21,13 @@ for PYTHON in \
     python3.2 \
     python3.3 \
     python3.4 \
+    python3.5 \
+    python3.6 \
     ;
 do
     if which $PYTHON >/dev/null
     then
+        echo "Info: Found ${PYTHON}"
         ${PYTHON} setup_makefile.py ${OS} tmp-$PYTHON.mak
         make -f tmp-$PYTHON.mak clean 2>&1 | tee tmp-$PYTHON.log
         make -f tmp-$PYTHON.mak test 2>&1 | tee -a tmp-$PYTHON.log
